29ac621fd011a6ad1fc7a390236292f8ca1867bc
cansilaci1 <mcsilaci@gmail.com>
2025-09-12 22:50:03 +0300

    updated axios parameters

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE || "http://localhost:3001"
+  baseURL: process.env.NEXT_PUBLIC_API_BASE || "https://mobiversite-api.onrender.com"
 });
 export default api;
